184aa605345b9183668ca1d7be9e9be81546fd3d
Ken Marchand <marchand_kenneth@smc.edu>
2020-08-30 02:35:53 -0700

    Added event handlers and button objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,90 @@
+
+var questions = [ {q: "Which of the following is not a Javascript data type? ",
+                    b1: "Numeric",
+                    b2: "String",
+                    b3: "Quantum",
+                    b4: "Boolean",
+                    bc1: false,
+                    bc2: false,
+                    bc3: true,
+                    bc4: false
+                },
+                    {q: "What is Javascript commonly used for?",
+                    b1: "Creating dynamic web pages",
+                    b2: "Creating Windows desktop applications",
+                    b3: "Creating Word documents",
+                    b4: "Any of the above"}
+                    ]
+var isAnswered = false;
+
 var button1 = "b1E1"
 var button2 = "b1E2"
 var button3 = "b1E3"
+var button4 = "b1E4"
 var body = document.body
+
+for (i=0;i<questions.length;i++){
+
 var h1E1 = document.createElement('h1')
-h1E1.textContent="Which of the following is not a Javascript data type?"
+h1E1.textContent=questions[0].q
 body.appendChild(h1E1)
 
 var button1=document.createElement('button')
-button1.textContent="String"
+button1.textContent=questions[0].b1
 body.appendChild(button1)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
 var button2=document.createElement('button')
-button2.textContent="Numeric"
+button2.textContent=questions[0].b2
 body.appendChild(button2)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
-var b1E3=document.createElement('button')
-b1E3.textContent="Quantum"
-body.appendChild(b1E3)
+var button3=document.createElement('button')
+button3.textContent=questions[0].b3
+body.appendChild(button3)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
-var b1E4=document.createElement('button')
-b1E4.textContent="Boolean"
-body.appendChild(b1E4)
+var button4=document.createElement('button')
+button4.textContent=questions[0].b4
+body.appendChild(button4)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
-button1.addEventListener('click',alertWrong,false);
-button2.addEventListener('click',alertWrong,false);
-b1E3.addEventListener('click', alertOK, false);
-b1E4.addEventListener('click',alertWrong,false)
+button1.addEventListener('click',alertWrong,true);                                            
+button2.addEventListener('click',alertWrong, true);
+button3.addEventListener('click',alertOK, true);
+button4.addEventListener('click',alertWrong,true);
+
+
 
 function alertOK(){
 var newNode = document.createElement('p');
 newNode.textContent="Right"
 body.appendChild(newNode)
+body.removeChild(button1)
+body.removeChild(button2)
+body.removeChild(button3)
+body.removeChild(button4)
+return;
 }
 
 function alertWrong(){
     var newNode = document.createElement('p');
     newNode.textContent="Wrong"
-    body.appendChild(newNode)
+    body.appendChild(newNode);
+    body.removeChild(button1)
+    body.removeChild(button2)
+    body.removeChild(button3)
+    body.removeChild(button4)
+    return;
     }
+
+
+}
