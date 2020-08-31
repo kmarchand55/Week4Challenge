dba1969e296a3ca25b1c594f93a46959df6faa9e
Ken Marchand <marchand_kenneth@smc.edu>
2020-08-30 17:03:53 -0700

    Still can't get it to work, added logic to ask questions, receive answers, and provide a message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,34 @@
-
+ 
 var questions = [ {q: "Which of the following is not a Javascript data type? ",
                     b1: "Numeric",
                     b2: "String",
                     b3: "Quantum",
                     b4: "Boolean",
-                    bc1: false,
-                    bc2: false,
-                    bc3: true,
-                    bc4: false
-                },
+                  bc1: "wrong",
+                  bc2: "wrong",
+                  bc3: "correct",
+                  bc4: "wrong"}
+                  
+                  
+                ,
                     {q: "What is Javascript commonly used for?",
                     b1: "Creating dynamic web pages",
                     b2: "Creating Windows desktop applications",
                     b3: "Creating Word documents",
                     b4: "Any of the above"}
                     ]
-var isAnswered = false;
-
 var button1 = "b1E1"
 var button2 = "b1E2"
 var button3 = "b1E3"
 var button4 = "b1E4"
 var body = document.body
-
-for (i=0;i<questions.length;i++){
+var i = 0
+console.log(questions[0].bc1)
 
 var h1E1 = document.createElement('h1')
 h1E1.textContent=questions[0].q
 body.appendChild(h1E1)
+console.log(questions[0].bc1)
 
 var button1=document.createElement('button')
 button1.textContent=questions[0].b1
@@ -57,34 +58,65 @@ body.appendChild(button4)
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
-button1.addEventListener('click',alertWrong,true);                                            
-button2.addEventListener('click',alertWrong, true);
-button3.addEventListener('click',alertOK, true);
-button4.addEventListener('click',alertWrong,true);
+button1.addEventListener('click',checkanswer());
+                
+button2.addEventListener('click',checkanswer());
+button3.addEventListener('click',checkanswer());
+button4.addEventListener('click',checkanswer());
 
 
 
+
+//console.log(questions[0].bc1)
+function checkanswer(){
+    if (questions[0].bc1 == "correct")
+    {alertOK()}
+   else
+   {alertWrong()}
+   if (questions[0].bc2 == "correct")
+    {alertOK()}
+   else
+   {alertWrong()}
+   if (questions[0].bc3 == "correct")
+    {alertOK()}
+   else
+   {alertWrong()}
+   if (questions[0].bc4 == "correct")
+    {alertOK()}
+   else
+   {alertWrong()}
+    //else if (questions[0].bc2 == "correct")
+   // {alertOK()}
+   // else if (questions[0].bc3 == "correct")
+   // {alertOK()}
+   // else if (questions[0].bc4 == "correct")
+   // {alertOK()}
+   // else
+   // {alertWrong()}
+   
+       
 function alertOK(){
-var newNode = document.createElement('p');
-newNode.textContent="Right"
-body.appendChild(newNode)
-body.removeChild(button1)
-body.removeChild(button2)
-body.removeChild(button3)
-body.removeChild(button4)
-return;
-}
-
-function alertWrong(){
     var newNode = document.createElement('p');
-    newNode.textContent="Wrong"
-    body.appendChild(newNode);
-    body.removeChild(button1)
-    body.removeChild(button2)
-    body.removeChild(button3)
-    body.removeChild(button4)
-    return;
+    newNode.textContent="Right"
+    body.appendChild(newNode)
+    //body.removeChild(button1)
+    //body.removeChild(button2)
+    //body.removeChild(button3)
+    //body.removeChild(button4)
     }
+    
+    function alertWrong(){
+        var newNode = document.createElement('p');
+        newNode.textContent="Wrong"
+        body.appendChild(newNode);
+        //body.removeChild(button1)
+        //body.removeChild(button2)
+        //body.removeChild(button3)
+        //body.removeChild(button4)
+           }
 
+           
+            }
+    
 
-}
+        
