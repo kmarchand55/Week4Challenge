fda21d04518941f70438b89a3190f0ee2ff33e0f
Ken Marchand <marchand_kenneth@smc.edu>
2020-08-30 19:15:20 -0700

    Final commit before submission, wish it were more complete, did my best

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,11 @@ var questions = [ {q: "Which of the following is not a Javascript data type? ",
                     b1: "Creating dynamic web pages",
                     b2: "Creating Windows desktop applications",
                     b3: "Creating Word documents",
-                    b4: "Any of the above"}
+                    b4: "Any of the above",
+                    bc1: "correct",
+                    bc2: "wrong",
+                    bc3: "wrong",
+                    bc4: "wrong"}
                     ]
 var button1 = "b1E1"
 var button2 = "b1E2"
@@ -23,42 +27,42 @@ var button3 = "b1E3"
 var button4 = "b1E4"
 var body = document.body
 var i = 0
-console.log(questions[0].bc1)
+console.log(questions[1].bc1)
 
 var h1E1 = document.createElement('h1')
-h1E1.textContent=questions[0].q
+h1E1.textContent=questions[1].q
 body.appendChild(h1E1)
-console.log(questions[0].bc1)
+console.log(questions[1].bc1)
 
 var button1=document.createElement('button')
-button1.textContent=questions[0].b1
+button1.textContent=questions[1].b1
 body.appendChild(button1)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
 var button2=document.createElement('button')
-button2.textContent=questions[0].b2
+button2.textContent=questions[1].b2
 body.appendChild(button2)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
 var button3=document.createElement('button')
-button3.textContent=questions[0].b3
+button3.textContent=questions[1].b3
 body.appendChild(button3)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
 var button4=document.createElement('button')
-button4.textContent=questions[0].b4
+button4.textContent=questions[1].b4
 body.appendChild(button4)
 
 var newNode = document.createElement('p');
 body.appendChild(newNode)
 
-button1.addEventListener('click',checkanswer());
+button1.addEventListener('click',alert("Correct"));
                 
 button2.addEventListener('click',checkanswer());
 button3.addEventListener('click',checkanswer());
@@ -69,19 +73,19 @@ button4.addEventListener('click',checkanswer());
 
 //console.log(questions[0].bc1)
 function checkanswer(){
-    if (questions[0].bc1 == "correct")
+    if (questions[1].bc1 == "correct")
     {alertOK()}
    else
    {alertWrong()}
-   if (questions[0].bc2 == "correct")
+   if (questions[1].bc2 == "correct")
     {alertOK()}
    else
    {alertWrong()}
-   if (questions[0].bc3 == "correct")
+   if (questions[1].bc3 == "correct")
     {alertOK()}
    else
    {alertWrong()}
-   if (questions[0].bc4 == "correct")
+   if (questions[1].bc4 == "correct")
     {alertOK()}
    else
    {alertWrong()}
@@ -99,6 +103,7 @@ function alertOK(){
     var newNode = document.createElement('p');
     newNode.textContent="Right"
     body.appendChild(newNode)
+    console.log(newNode.textContent)
     //body.removeChild(button1)
     //body.removeChild(button2)
     //body.removeChild(button3)
